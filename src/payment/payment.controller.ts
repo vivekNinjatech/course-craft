f71b943534b2cada521bd85080c7a12cdc84526e
vivekNinjatech <vivek@ninjatechnolabs.com>
2025-01-13 18:58:37 +0530

    add: controllers, services and dto for payment

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Patch, Post } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { CreatePaymentDto, GetPaymentDto, UpdatePaymentStatusDto } from './dto';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private paymentService: PaymentService) {}
+
+  @HttpCode(201)
+  @Post('create')
+  async createPayment(dto: CreatePaymentDto) {
+    return this.paymentService.createPayment(dto);
+  }
+
+  @HttpCode(200)
+  @Patch('update')
+  async updatePayment(dto: UpdatePaymentStatusDto) {
+    return this.paymentService.updatePayment(dto);
+  }
+
+  @HttpCode(200)
+  @Get('get')
+  async getPayment(dto: GetPaymentDto) {
+    return this.paymentService.getPayment(dto);
+  }
+}
